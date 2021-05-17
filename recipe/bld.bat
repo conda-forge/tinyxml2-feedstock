56c820a3eace2c5ee517618eccd5311141c9d6e6
Chris Burr <chrisburr@users.noreply.github.com>
2021-05-17 11:31:15 +0200

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,6 @@
 mkdir build
 cd build
-cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DBUILD_SHARED_LIBS ON ..\source
+cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D BUILD_SHARED_LIBS=ON ..\source
 if errorlevel 1 exit 1
 nmake install
 if errorlevel 1 exit 1
